32227407a92eaeb2be1024f3095d1409788c54f0
Bipin2020100 <b.regmi2014@gmail.com>
2021-06-07 17:54:27 -0500

    update

--- client/src/app/posts-module/comments.component.ts
+++ client/src/app/posts-module/comments.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { IComments, IUser, newIUser } from '../app.types';
+import { MainServiceService } from './main-service.service';
 
 
 @Component({
@@ -9,7 +10,18 @@ import { IComments, IUser, newIUser } from '../app.types';
 
   <div class="comment">
   <div class="text desc">{{comment.comment}}</div>
-  <div class="text">{{comment.user.username}} on {{comment.date | date: "M/d/yy h:mm a"}}</div>
+  <button class ="userBtn" (click)="displayProfile(comment.user._id)">{{comment.user.username}}</button>
+  
+  <div class ="userInfo" *ngIf="showCommentUser">
+    <div>Name : {{comment.user.name}}</div>
+    <div>Address : {{comment.user.address}}</div>
+    <div>City : {{comment.user.city}}</div>
+    <div>State : {{comment.user.state}}</div>
+    <div>ZipCode : {{comment.user.zip}}</div>
+    <div>Phone : {{comment.user.phone}}</div>
+    <div>Email : {{comment.user.email}}</div>
+  </div>
+    <div class="text"> on {{comment.date | date: "M/d/yy h:mm a"}}</div>
   </div>
   <br>
   `,
@@ -18,15 +30,20 @@ import { IComments, IUser, newIUser } from '../app.types';
            ".comment {padding: 20px;}",
            ".comment {border-radius: 25px;}",
            ".text {color: white; padding: 5px}",
-           ".desc {font-size: 150%;}"
+           ".desc {font-size: 150%;}",
+           ".userInfo {color: white}",
+           ".userBtn {background-color:dodgerBlue;color :white; border:none; padding :5px; border-radius :10%}"
 
           ],
 })
 export class CommentsComponent implements OnInit {
   @Input() comment : IComments
+  
+  showCommentUser : boolean =false;
+
 
   newComment : FormControl= new FormControl('',Validators.required)
-  constructor() {
+  constructor(private myService: MainServiceService) {
 
     this.comment = {
       comment: "",
@@ -34,6 +51,10 @@ export class CommentsComponent implements OnInit {
       date: "",
     }
 
+  }
+  
+  displayProfile(id: string){
+    this.showCommentUser = !this.showCommentUser
   }
 
   ngOnInit(): void {}
